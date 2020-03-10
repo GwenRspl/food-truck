22678e52c62d5f5630fc91d306e0dbe4e231bfa6
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-10 17:00:56 +0100

    wip sql

--- src/main/java/com/example/demo/repository/OrderLineRepository.java
+++ src/main/java/com/example/demo/repository/OrderLineRepository.java
@@ -8,4 +8,9 @@ import com.example.demo.model.OrderLine;
 @Repository
 public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
 
+	// select order_line.product_id, name, price, image, grade from
+	// foodtruck.order_line inner join foodtruck.product on product_id=product.id
+	// left join foodtruck.rating on order_line.product_id=rating.product_id group
+	// by product_id order by sum(quantity) desc limit 3;
+
 }
